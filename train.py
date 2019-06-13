0c4260c6da5a94a3c7669435b1e8746dc9e1adbd
Murphy Huang <huangyhq@gmail.com>
2019-06-13 14:27:52 -0400

    add some support for running training on server

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
+import os
 import tensorflow as tf
 
 from pct.bin import pct_trainer
@@ -16,6 +17,10 @@ def main(argv):
 
 if __name__ == "__main__":
 
+  config_path = os.path.join(
+    os.path.dirname(__file__),
+    'pct/test_data/libigpu0_hparams.yml'
+  )
   sys_argv = """
   --config_dir=/home/murphyhuang/dev/src/github.com/EstelleHuang666/PCT-dev/pct/test_data/hparams.yml
   """
